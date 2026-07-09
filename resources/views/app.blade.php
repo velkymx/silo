ae3da097f0a91a31caaa61627693fbbd55e32acd
Alan B <ajb@wow.com>
2026-07-09 13:20:11 -0700

    feat(brand): rocket favicon
    Serve the header brand mark (Bootstrap Icons rocket-takeoff-fill in the
    app primary color) as an SVG favicon, linked from the app layout.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title inertia>{{ config('app.name', 'Silo') }}</title>
 
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg">
 
     @vite('resources/js/app.js')
     @inertiaHead
